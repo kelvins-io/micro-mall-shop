66d902d643fdbd55ba8a357f6047690d0b70f2a5
kelvins-io <mybaishati@gmail.com>
2021-05-22 00:06:06 +0800

    del not used amqp

--- startup/vars.go
+++ startup/vars.go
@@ -1,25 +1,8 @@
 package startup
 
-import (
-	"gitee.com/cristiane/micro-mall-shop/vars"
-	"gitee.com/kelvins-io/kelvins/setup"
-)
-
 // SetupVars 加载变量
 func SetupVars() error {
 	var err error
-	if vars.QueueAMQPSettingUserRegisterNotice != nil && vars.QueueAMQPSettingUserRegisterNotice.Broker != "" {
-		vars.QueueServerUserRegisterNotice, err = setup.NewAMQPQueue(vars.QueueAMQPSettingUserRegisterNotice, nil)
-		if err != nil {
-			return err
-		}
-	}
 
-	if vars.QueueAMQPSettingUserStateNotice != nil && vars.QueueAMQPSettingUserStateNotice.Broker != "" {
-		vars.QueueServerUserStateNotice, err = setup.NewAMQPQueue(vars.QueueAMQPSettingUserStateNotice, nil)
-		if err != nil {
-			return err
-		}
-	}
 	return err
 }
